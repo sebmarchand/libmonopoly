373e674816e92fbd668ced4e3d53836ac6d2983a
Simon Marchi <simon.marchi@polymtl.ca>
2012-12-13 12:07:39 -0500

    Fix whitespaces
    Spaces to tabs and trailing whitespaces.
    
    Signed-off-by: Simon Marchi <simon.marchi@polymtl.ca>

--- src/Monopoly.php
+++ src/Monopoly.php
@@ -6,7 +6,7 @@
 		public $code;	// Code étudiant
 		public $pwd;	// Mot de passe
 		public $nais;	// Date de naissance (format JJ/MM/AA)
-		
+
 		/**
 		 * Constructeur - raccourci pour fixer les valeurs.
 		 *
@@ -20,7 +20,7 @@
 			$this->nais = $nais;
 		}
 	}
-	
+
 	/**
 	 * Statut d'un cours.
 	 */
@@ -32,7 +32,7 @@
 		public $type;		// Type (théorique ou laboratoire)
 		public $gr;		// Numéro de groupe
 	}
-	
+
 	/**
 	 * Information sur un cours.
 	 */
@@ -43,17 +43,17 @@
 		public $is_internship;		// Vrai si est un stage
 		public $available_types;	// Tableau de types de cours disponibles
 	}
-	
+
 	/**
 	 * Horaire d'un cours.
 	 */
-	class MonopolySched {	
+	class MonopolySched {
 		public $pernum;		// Numéro/code de période
 		public $day_of_week;	// Journée de la semaine (0 pour lundi)
 		public $h;		// Heure (format 24h)
 		public $m;		// Minute
 	}
-	
+
 	/**
 	 * Cours.
 	 */
@@ -76,14 +76,14 @@
 		const K_VALUE = 'value';
 		const COURSE_LAB = 'L';		// Clef à utiliser pour un groupe de laboratoire
 		const COURSE_THEO = 'T';	// Clef à utiliser pour un groupe théorique
-		
+
 		private $_validation_raw_data = NULL;		// Données brutes de la page de validation
 		private $_choixcours_raw_data = NULL;		// Données brutes de la page de modification de choix de cours
 		private $_presentation_raw_data = NULL;		// Données brutes de la page de présentation des résultats académiques finaux
 		private $_registered_courses = NULL;		// Tableau des cours enregistrés
 		private $_new_registered_courses = NULL;	// Tableau des nouveaux cours enregistrés
 		private $_config_vo = NULL;
-		
+
 		/**
 		 * Construit une chaine POST à partir d'un tableau.
 		 *
@@ -93,7 +93,7 @@
 		 */
 		private function build_post_string($post, $look_at = NULL) {
 			$ret = "";
-		
+
 			foreach ($post as $k => $v) {
 				$name = urlencode($k);
 				if (is_null($look_at)) {
@@ -103,10 +103,10 @@
 				}
 				$ret .= "$name=$value&";
 			}
-		
+
 			return substr($ret, 0, strlen($ret) - 1);
 		}
-		
+
 		/**
 		 * Émet une requête POST avec cURL.
 		 *
@@ -116,7 +116,7 @@
 		 * @return		Résultat renvoyé par le serveur
 		 */
 		private function curl_post($url, $post_string, $curl_opts = array()) {
-			$ch = curl_init($url);			
+			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post_string);
 			curl_setopt($ch, CURLOPT_POST, TRUE);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
@@ -126,10 +126,10 @@
 			}
 			$res = curl_exec($ch);
 			curl_close($ch);
-			
+
 			return $res;
-		}		
-		
+		}
+
 		/**
 		 * Met en cache les données brutes de validation.
 		 */
@@ -144,7 +144,7 @@
 				$this->_validation_raw_data = $this->curl_post(self::VALIDATION_URL, $ps);
 			}
 		}
-		
+
 		/**
 		 * Met en cache les données brutes d'une page à partir des entrées de la page de validation.
 		 *
@@ -160,7 +160,7 @@
 				$to_set = $this->curl_post($url, $ps);
 			}
 		}
-		
+
 		/**
 		 * Met en cache les données brutes de la page de mofication de choix de cours.
 		 *
@@ -169,7 +169,7 @@
 		private function set_choixcours_raw_data($force = false) {
 			$this->set_raw_data_from_validation($this->_choixcours_raw_data, self::CHOIXCOURS_URL, $force);
 		}
-		
+
 		/**
 		 * Met en cache les données brutes de la page de présentation des résultats académiques finaux.
 		 *
@@ -178,7 +178,7 @@
 		private function set_presentation_raw_data($force = false) {
 			$this->set_raw_data_from_validation($this->_presentation_raw_data, self::PRESENTATION_URL, $force);
 		}
-		
+
 		/**
 		 * Obtenir les entrées de formulaire d'une page.
 		 *
@@ -202,10 +202,10 @@
 					self::K_VALUE => $value
 				);
 			}
-		
+
 			return $ret;
 		}
-		
+
 		/**
 		 * Obtenir la valeur d'un attribut d'une balise XML.
 		 *
@@ -221,10 +221,10 @@
 					}
 				}
 			}
-		
+
 			return $m[1];
 		}
-		
+
 		/**
 		 * Construit le gestionnaire à partir d'une configuration d'étudiant.
 		 *
@@ -238,7 +238,7 @@
 				$this->set_validation_raw_data();
 			}
 		}
-		
+
 		/**
 		 * Vérifie si le système distant est en ligne.
 		 *
@@ -248,7 +248,7 @@
 			$this->set_validation_raw_data();
 			return strstr($this->_validation_raw_data, '<font size="+2">Cliqu</font><font size="+2">ez sur la fonction d&eacute;sir&eacute;e</font>') !== FALSE;
 		}
-		
+
 		/**
 		 * Obtenir un VO d'horaire à partir d'un code de période.
 		 *
@@ -259,7 +259,7 @@
 			$pernum = trim($pernum);
 			$p1 = $pernum[0];
 			$p2 = $pernum[1];
-			
+
 			$sched_vo = new MonopolySched;
 			$sched_vo->pernum = $pernum;
 			$sched_vo->day_of_week = $p1 - 1;
@@ -269,10 +269,10 @@
 			} else {
 				$sched_vo->m = 45;
 			}
-			
+
 			return $sched_vo;
 		}
-		
+
 		/**
 		 * Obtenir le statut d'un cours.
 		 *
@@ -283,7 +283,7 @@
 		 */
 		public function get_course_status($abbr, $type, $gr) {
 			$this->set_choixcours_raw_data();
-			
+
 			$r_gr = str_pad($gr, 2, "0", STR_PAD_LEFT);
 			$r_abbr = strtoupper($abbr);
 			$r_type = strtoupper($type);
@@ -310,10 +310,10 @@
 			$course_status_vo->gr = $gr;
 			$course_status_vo->places = $places;
 			$course_status_vo->h = $h;
-			
+
 			return $course_status_vo;
 		}
-		
+
 		/**
 		 * Obtenir les informations d'un cours.
 		 *
@@ -322,7 +322,7 @@
 		 */
 		public function get_course_info($abbr) {
 			$this->set_choixcours_raw_data();
-			
+
 			$r_abbr = strtoupper($abbr);
 			$to_match = sprintf("/this\s*\[\s*\"%s\"\s*\]\s*=\s*\"([^\"]+)\"\s*;/i", $r_abbr);
 			if (!preg_match($to_match, $this->_choixcours_raw_data, $m)) {
@@ -334,7 +334,7 @@
 			$type = substr($m[1], 4, 3);
 			$is_final_proj = (substr($m[1], 1, 1) == 'O');
 			$is_int = (substr($m[1], 0, 1) == 'O');
-		
+
 			$course_info_vo = new MonopolyCourseInfo;
 			$course_info_vo->name = $name;
 			$course_info_vo->credits = $cr;
@@ -345,10 +345,10 @@
 			}
 			$course_info_vo->is_final_project = $is_final_proj;
 			$course_info_vo->is_internship = $is_int;
-			
+
 			return $course_info_vo;
 		}
-		
+
 		/**
 		 * Fixe le tableau interne des cours enregistrés.
 		 *
@@ -362,7 +362,7 @@
 			$ret = array();
 			$this->set_choixcours_raw_data(true);
 			$inputs = $this->get_inputs_from_raw_data($this->_choixcours_raw_data);
-		
+
 			for ($i = 1; $i <= 10; ++$i) {
 				if (isset($inputs["sigle$i"]['value'])) {
 					if (strlen($z = $inputs["sigle$i"]['value']) > 0) {
@@ -380,11 +380,11 @@
 					}
 				}
 			}
-		
+
 			$this->_registered_courses = $ret;
 			return true;
 		}
-		
+
 		/**
 		 * Trouve l'emplacement d'un cours dans le tableau interne des cours enregistrés.
 		 *
@@ -394,7 +394,7 @@
 		private function find_registered_course($course) {
 			$this->set_registered_courses();
 			$course->abbr = strtoupper($course->abbr);
-			
+
 			$flag = -1;
 			foreach ($this->_registered_courses as $k => $c) {
 				$match = true;
@@ -418,7 +418,7 @@
 				return false;
 			}
 		}
-		
+
 		/**
 		 * Trouve l'emplacement d'un cours dans le tableau interne des cours enregistrés.
 		 *
@@ -430,10 +430,10 @@
 			$course->abbr = strtoupper($abbr);
 			$course->gr_theo = NULL;
 			$course->gr_lab = NULL;
-			
+
 			return $this->find_registered_course($course);
 		}
-		
+
 		/**
 		 * Prépare les entrées de formulaire pour une soumission des cours enregistrés.
 		 *
@@ -443,11 +443,11 @@
 			$nbr_cours = 0;
 			$nbr_cr = 0;
 			$nbr_cr_wo_pfe = 0;
-			
+
 			$this->set_registered_courses();
 			$inputs = $this->get_inputs_from_raw_data($this->_choixcours_raw_data);
-			
-			for ($i = 1; $i <= 10; ++$i) {				
+
+			for ($i = 1; $i <= 10; ++$i) {
 				$inputs["sigle$i"]['value'] = "";
 				$inputs["grtheo$i"]['value'] = "";
 				$inputs["grlab$i"]['value'] = "";
@@ -460,7 +460,7 @@
 				$inputs["Igrlab$i"]['value'] = "";
 			}
 			$i = 1;
-			foreach ($this->_registered_courses as $course) {				
+			foreach ($this->_registered_courses as $course) {
 				$course_info = $this->get_course_info($course->abbr);
 				if (is_null($course_info)) {
 					return false;
@@ -476,13 +476,13 @@
 				if (!in_array($course->abbr, $this->_new_registered_courses)) {
 					$inputs["Igrtheo$i"]['value'] = $inputs["grtheo$i"]['value'];
 					$inputs["Igrlab$i"]['value'] = $inputs["grlab$i"]['value'];
-				}				
+				}
 				++$nbr_cours;
 				$nbr_cr += $course_info->credits;
 				if (!$course_info->is_final_project) {
 					$nbr_cr_wo_pfe += $course_info->credits;
 				}
-				
+
 				++$i;
 			}
 			$inputs['bascule']['value'] = '';
@@ -490,10 +490,10 @@
 			$inputs['nbr_cours']['value'] = $nbr_cours;
 			$inputs['totalCredits']['value'] = $nbr_cr;
 			$inputs['totalCreditsSansPFE']['value'] = $nbr_cr_wo_pfe;
-			
+
 			return $inputs;
 		}
-		
+
 		/**
 		 * Obtenir une copie des cours enregistrés à date.
 		 *
@@ -501,14 +501,14 @@
 		 */
 		public function get_registered_courses() {
 			$this->set_registered_courses();
-			
+
 			foreach ($this->_registered_courses as $k => $v) {
 				$ret[$k] = clone $v;
 			}
-			
+
 			return $ret;
 		}
-		
+
 		/**
 		 * Retirer un cours enregistré de la cache interne.
 		 *
@@ -526,11 +526,11 @@
 					$keys = array_keys($this->_new_registered_courses);
 					unset($this->_new_registered_courses[$keys[0]]);
 				}
-				
+
 				return true;
 			}
 		}
-		
+
 		/**
 		 * Ajoute un cours aux cours enregistrés internes.
 		 *
@@ -564,10 +564,10 @@
 					return true;
 				}
 			}
-			
+
 			return false;
 		}
-		
+
 		/**
 		 * Soumet les cours enregistrés pour une modification officielle des choix de cours;
 		 * attention, l'étudiant a droit à un nombre maximal par session et cette méthode,
@@ -578,20 +578,20 @@
 			$ps = $this->build_post_string($inputs, 'value');
 			$this->curl_post(self::MODIFCOURS_URL, $ps);
 		}
-		
+
 		/**
 		 * Obtient un tableau associatif des résultats académiques finaux.
 		 *
 		 * @return	Résultats académiques finaux
-		 */		
+		 */
 		public function get_final_grades() {
 			$this->set_presentation_raw_data();
-			
+
 			preg_match_all('/<td width="125"><div align="right"><font size="1" face="Courier New", "Courier", "monospace">([^<]*)<\/font>/', $this->_presentation_raw_data, $abbrs);
 			preg_match_all('/<td width="63"><div align="center"><font size="1" face="Courier New", "Courier", "monospace">([^<]*)<\/font>/', $this->_presentation_raw_data, $grades);
 			$abbrs = $abbrs[1];
 			$grades = $grades[1];
-		
+
 			foreach($abbrs as $k => $v) {
 				$abbrs[$k] = strtoupper(trim($abbrs[$k]));
 			}
